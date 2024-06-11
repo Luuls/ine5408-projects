d19c803c43cbd00a4317fcc3e479a6d42d0ad31e
Luuls <luan13467@hotmail.com>
2024-06-11 01:10:16 -0300

    feat: implement Controller::readFile method

--- project-II/include/controller.h
+++ project-II/include/controller.h
@@ -1,12 +1,9 @@
 #pragma once
 
 #include <string>
-#include <iostream>
-#include <vector>
 
 #include "trie.h"
 
-// talvez essa classe seja apagada
 
 // classe para gerenciar a Trie (construção, aplicação, etc)
 class Controller {
