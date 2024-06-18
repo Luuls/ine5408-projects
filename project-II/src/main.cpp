bb6a02cb0b0dbdb39c8e04458810dcdd89517510
Luuls <luan13467@hotmail.com>
2024-06-18 16:12:22 -0300

    style: remove unused headers in main.cpp

--- project-II/src/main.cpp
+++ project-II/src/main.cpp
@@ -1,20 +1,9 @@
-<<<<<<< Updated upstream
-#include <iostream>
-#include <sstream>
-#include <fstream>
-#include <iterator>
-
-=======
->>>>>>> Stashed changes
-#include "trie.h"
 #include "controller.h"
 
 
 int main(int argc, char* argv[]) {
     Controller controller;
     controller.buildTrie(argv[1]);
-    Trie& trie = controller.getTrie();
-    trie.print();
 
     return 0;
 }
