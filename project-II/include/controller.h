9627167cbf1e41713047bdd233271ac35923bebd
Luuls <luan13467@hotmail.com>
2024-06-24 18:25:39 -0300

    docs: add documentation to all symbols. Also fix some code styling and minor refactoring

--- project-II/include/controller.h
+++ project-II/include/controller.h
@@ -5,18 +5,35 @@
 #include "trie.h"
 
 
-// classe para gerenciar a Trie (construção, aplicação, etc)
+/**
+ * @class Controller
+ * @brief Controla a leitura do arquivo de dicionário e a interação com o usuário.
+ *
+ * A classe Controller lê o conteúdo de um arquivo de dicionário e insere as palavras
+ * na trie. Ela também lê palavras do console e imprime informações sobre elas,
+ * como descrito no enunciado.
+ */
 class Controller {
 public:
     Controller();
 
+    /**
+     * @brief Lê o conteúdo de um arquivo de dicionário e
+     * insere as palavras na trie.
+     * @param filename O nome do arquivo a ser lido.
+     */
     void buildTrie(const std::string& filename);
 
     Trie& getTrie();
 
+    /**
+     * @brief Lê palavras do console e imprime informações sobre elas,
+     * como descrito no enunciado.
+     */
     void handleInput();
 
 private:
+    // métodos auxiliares
     std::string readFile(const std::string& filename);
     std::string readWord(std::string::iterator& it);
 
--- project-II/src/trie.cpp
+++ project-II/src/trie.cpp
@@ -1,9 +1,10 @@
+#include <iostream>
 #include <vector>
 
-#include "../include/trie.h"
+#include "trie.h"
 
 Trie::Trie() : letter(0), dictPosition(0), lineLength(0) {
-    for (int i = 0; i < 26; i++) {
+    for (int i = 0; i < 27; i++) {
         children[i] = nullptr;
     }
 }
@@ -19,6 +20,7 @@ Trie::Trie(char letter, unsigned int dictPosition, unsigned int lineLength) :
 Trie::~Trie() {
     for (int i = 0; i < 26; i++) {
         if (children[i] != nullptr) {
+            // recursivamente deleta os filhos
             delete children[i];
         }
     }
@@ -26,8 +28,10 @@ Trie::~Trie() {
 
 Trie::RetrievalResult Trie::retrieve(const std::string& word) {
     Trie* child = this;
+    // navega até o nó que representa a última letra da palavra
     for (const auto& letter : word) {
         child = child->children[letter - 'a'];
+        // se a palvra não existir, retorna (0, 0)
         if (child == nullptr) {
             return { 0, 0 };
         }
@@ -42,14 +46,17 @@ std::vector<std::string> Trie::getWordsWithPrefix(const std::string& prefix) {
     if (prefix.empty()) {
         // define-se que a string vazia é prefixo de qualquer palavra
         // portanto, a Trie retorna todas as suas palavras
-        this->getWords("", words);
+        this->getWords(words);
         return words;
     }
 
     Trie* child = this;
     if (prefix.size() > 1) {
+        // navega até o nó que representa a penúltima letra do prefixo
         for (const auto& letter : prefix.substr(0, prefix.size() - 1)) {
             child = child->children[letter - 'a'];
+            // se algum nó intermediário for nulo, significa
+            // que o prefixo não existe dentro da trie
             if (child == nullptr) {
                 return words;
             }
@@ -58,36 +65,47 @@ std::vector<std::string> Trie::getWordsWithPrefix(const std::string& prefix) {
 
     char lastLetter = prefix.at(prefix.size() - 1);
     child = child->children[lastLetter - 'a'];
-    if (child != nullptr) {
-        if (child->lineLength != 0) {
-            words.push_back(prefix);
-        }
-    }
-    else {
+    if (child == nullptr) {
         return words;
     }
 
-    child->getWords(prefix, words);
+    // se o tamanho da linha do nó referente à
+    // última letra do prefixo for diferente de 0,
+    // então o prefixo também é uma palavra do dicionário
+    if (child->lineLength != 0) {
+        words.push_back(prefix);
+    }
+
+    // à partir do prefixo, recupera todas as palavras,
+    // pois todas começam com `prefix`
+    child->getWords(words, prefix);
     return words;
 }
 
-void Trie::getWords(std::string currentWord, std::vector<std::string>& words) {
+void Trie::getWords(std::vector<std::string>& words, std::string currentWord) {
     for (const auto& child : this->children) {
         if (child != nullptr) {
             std::string nextWord = currentWord + child->getLetter();
+            // se o tamanho da linha do nó for diferente de 0,
+            // então nextWord é uma palavra do dicionário
             if (child->getLineLength() != 0) {
                 words.push_back(nextWord);
             }
-            child->getWords(nextWord, words);
+            child->getWords(words, nextWord);
         }
     }
 }
 
-void Trie::insert(const std::string& word, unsigned int dictPosition, unsigned int lineLength) {
+void Trie::insert(
+                  const std::string& word,
+                  unsigned int dictPosition,
+                  unsigned int lineLength) {
     if (word.empty()) {
         return;
     }
 
+    // a palavra inserida é consumida da esquerda para a direita
+    // a cada chamada à insert, até que fique vazia
     char letter = word[0];
     size_t letterIndex = letter - 'a';
     auto& child = this->children[letterIndex];
@@ -97,6 +115,7 @@ void Trie::insert(const std::string& word, unsigned int dictPosition, unsigned i
     }
 
     child->setLetter(letter);
+    // se o tamanho da palavra for 1, o nó é a última letra da palavra original
     if (word.size() == 1) {
         child->setDictPosition(dictPosition);
         child->setLineLength(lineLength);
@@ -117,3 +136,16 @@ void Trie::setLineLength(unsigned int lineLength) {
     this->lineLength = lineLength;
 }
 unsigned int Trie::getLineLength() const { return this->lineLength; }
+
+void Trie::print(const std::string& prefix) {
+    if (letter != 0) { // não imprime o caractere da raíz
+        std::cout << prefix
+            << letter
+            << " (" << dictPosition << ", " << lineLength << ")\n";
+    }
+    for (auto child : children) {
+        if (child) {
+            child->print(prefix + "--");
+        }
+    }
+}
